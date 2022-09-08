e359098000ed837b6447e303371cbd5d8e1a1eb4
Adonis Ling <adonis0147@gmail.com>
2022-09-08 16:23:26 +0800

    Add basic parsers

--- include/llparser.h
+++ include/llparser.h
@@ -1,3 +1,114 @@
 #pragma once
 
-namespace llparser {}  // namespace llparser
+#include <algorithm>
+#include <any>
+#include <cstddef>
+#include <iterator>
+#include <regex>
+#include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+namespace llparser {
+
+struct ParseResult {
+   public:
+    enum Status { SUCCESS, FAILURE };
+
+    bool is_success() { return status == SUCCESS; }
+
+    template <typename T>
+    T get() {
+        return std::any_cast<T>(value);
+    }
+
+    Status status;
+    size_t index;
+    std::any value;
+};
+
+template <typename T>
+class ObjectAllocator {
+   public:
+    ~ObjectAllocator() {
+        for (const auto* object : objects) {
+            delete object;
+        }
+    }
+
+    template <typename... Ts>
+    T* allocate(Ts&&... args) {
+        auto* object = new T(std::forward<Ts>(args)...);
+        objects.push_back(object);
+        return object;
+    }
+
+   private:
+    std::vector<T*> objects;
+};
+
+class LLParser {
+   public:
+    using Status = ParseResult::Status;
+    using Attachment = std::any;
+    using ParseFunction = ParseResult (*)(const LLParser*, const std::string&, size_t);
+
+    template <typename T>
+    LLParser(T&& attachment, const ParseFunction parse_function)
+        : _attachment(std::forward<T>(attachment)), _parse(parse_function) {}
+
+    LLParser(const LLParser&) = delete;
+    LLParser& operator=(const LLParser&) = delete;
+
+    const Attachment& attachment() const { return _attachment; }
+
+    ParseResult parse(const std::string& text) const { return _parse(this, text, 0); }
+
+    ParseResult parse(const std::string& text, size_t start) const {
+        return _parse(this, text, start);
+    }
+
+    template <typename Allocator>
+    static const LLParser* string(std::string_view literal, Allocator* allocator) {
+        return _allocate<LLParser>(
+            allocator, std::string(literal),
+            [](const auto* parser, const auto& text, auto start) -> auto{
+                const auto& literal = std::any_cast<std::string>(parser->attachment());
+                const auto& begin = std::cbegin(text) + start;
+                size_t length = std::cend(text) - begin;
+                if (literal.compare(0, literal.length(), text, start,
+                                    std::min(literal.length(), length)) == 0) {
+                    return ParseResult{Status::SUCCESS, start + literal.length(),
+                                       std::string(begin, begin + literal.length())};
+                } else {
+                    return ParseResult{Status::FAILURE, start, nullptr};
+                }
+            });
+    }
+
+    template <typename Allocator>
+    static const LLParser* regex(std::string_view pattern, Allocator* allocator) {
+        return _allocate<LLParser>(
+            allocator, std::regex(std::string(pattern)),
+            [](const auto* parser, const auto& text, auto start) -> auto{
+                const auto& pattern = std::any_cast<std::regex>(parser->attachment());
+                std::smatch match;
+                if (std::regex_search(std::cbegin(text) + start, std::cend(text), match, pattern)) {
+                    return ParseResult{Status::SUCCESS, start + match.length(), match.str()};
+                } else {
+                    return ParseResult{Status::FAILURE, start, nullptr};
+                }
+            });
+    }
+
+   private:
+    template <typename T, typename Allocator, typename... Ts>
+    static T* _allocate(Allocator* allocator, Ts&&... args) {
+        return reinterpret_cast<T*>(allocator->allocate(std::forward<Ts>(args)...));
+    }
+    const Attachment _attachment;
+    const ParseFunction _parse;
+};
+
+}  // namespace llparser
--- test/llparser_test.cc
+++ test/llparser_test.cc
@@ -1,1 +1,61 @@
 #include <gtest/gtest.h>
+#include <llparser.h>
+
+#include <any>
+#include <cstddef>
+#include <cstring>
+#include <string>
+
+namespace llparser {
+
+using Status = ParseResult::Status;
+
+TEST(llparse_test, test_string_parser) {
+    ObjectAllocator<LLParser> allocator;
+    const auto* parser = LLParser::string("Hello, world!", &allocator);
+    std::string text = "Hello, world!";
+    auto result = parser->parse(text);
+    ASSERT_EQ(Status::SUCCESS, result.status);
+    ASSERT_EQ(text.length(), result.index);
+    ASSERT_EQ(text, std::any_cast<std::string>(result.value));
+
+    text = "hello, world!";
+    result = parser->parse(text);
+    ASSERT_EQ(Status::FAILURE, result.status);
+    ASSERT_EQ(0, result.index);
+    ASSERT_EQ(nullptr, std::any_cast<nullptr_t>(result.value));
+
+    text = "hello, world! Hello, world!";
+    result = parser->parse(text, strlen("hello, world! "));
+    ASSERT_EQ(Status::SUCCESS, result.status);
+    ASSERT_EQ(text.length(), result.index);
+    ASSERT_EQ("Hello, world!", std::any_cast<std::string>(result.value));
+
+    result = parser->parse(text, text.length() - 1);
+    ASSERT_EQ(Status::FAILURE, result.status);
+    ASSERT_EQ(text.length() - 1, result.index);
+    ASSERT_EQ(nullptr, std::any_cast<nullptr_t>(result.value));
+}
+
+TEST(llparse_test, test_regex_parser) {
+    ObjectAllocator<LLParser> allocator;
+    const auto* parser = LLParser::regex("^\\d+", &allocator);
+    std::string text = "123456";
+    auto result = parser->parse(text);
+    ASSERT_EQ(Status::SUCCESS, result.status);
+    ASSERT_EQ(text.length(), result.index);
+    ASSERT_EQ(text, std::any_cast<std::string>(result.value));
+
+    text = "a123456";
+    result = parser->parse(text);
+    ASSERT_EQ(Status::FAILURE, result.status);
+    ASSERT_EQ(0, result.index);
+    ASSERT_EQ(nullptr, std::any_cast<nullptr_t>(result.value));
+
+    result = parser->parse(text, text.length() - 1);
+    ASSERT_EQ(Status::SUCCESS, result.status);
+    ASSERT_EQ(text.length(), result.index);
+    ASSERT_EQ("6", std::any_cast<std::string>(result.value));
+}
+
+}  // namespace llparser
